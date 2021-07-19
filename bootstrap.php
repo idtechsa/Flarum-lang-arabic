ffb798a961fe76e4dbfb5745d3eae28013d20977
idtechsa <62919422+idtechsa@users.noreply.github.com>
2021-07-20 00:28:19 +0300

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,5 @@
 <?php
+namespace Flarumarabic\Arabic;
 
 use Flarum\Event\ConfigureLocales;
 use Flarum\Event\ConfigureClientView;
@@ -6,25 +7,36 @@ use Flarum\Forum\WebApp;
 use Flarum\Locale\LocaleManager;
 use Illuminate\Contracts\Events\Dispatcher;
 
+
+return function (Dispatcher $events) {
+	$events->subscribe(Listener\RTLClientView::class);
+};
 return function (Dispatcher $events) {
     $events->listen(ConfigureLocales::class, function (ConfigureLocales $event) {
         $event->loadLanguagePackFrom(__DIR__);
     });
 
     //Add assets
-    $events->listen(ConfigureClientView::class,  function(ConfigureClientView $event) {
-      $webapp = app(WebApp::class);
-	$assets = $webapp-> getAssets();
-	$assets->flushLocaleCss();
-      $locales = app(LocaleManager::class);
+$events->listen(ConfigureClientView::class,  function(ConfigureClientView $event) {
 
-      if ($event->isForum() && $locales->getLocale() == "ar") {
-           $event->addAssets([
-              __DIR__ . '/less/forum/extension.less'
-           ]);
-	   // change the html dir tag from ltr to rtl 
-	   $event->view->direction = 'rtl';   
-        }
-    });
-};
+  $webapp = app(WebApp::class);
+  $assets = $webapp->getAssets();
+  $assets->flushLocaleCss();
 
+  $locales = app(LocaleManager::class);
+  $isArabic = $locales->getLocale() == "ar";
+  // dd($event->view->direction = 'rtl');
+
+  if ($event->isForum() && $isArabic   ) {
+
+     $event->addAssets([__DIR__ . '/less/forum/extension.less']);
+     $event->view->direction = 'rtl';
+
+  } elseif ($event->isAdmin() && $isArabic) {
+
+     $event->addAssets([__DIR__ . '/less/admin/extension.less']);
+     $event->view->direction = 'rtl';
+
+  }
+
+});
